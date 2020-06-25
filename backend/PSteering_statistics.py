99ccfcf4253d9b9c4e37a59e969daee4326e2748
Marco Angelini <you@example.com>
2020-06-25 10:05:35 +0200

    first statistics script version

---
+++ backend/PSteering_statistics.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from bokeh.io import export_png
+from bokeh.models import Span
+from bokeh.models import Legend
+import os
+
+filename="Evaluation/completeLog.csv"
+f.open(filename,"r")
