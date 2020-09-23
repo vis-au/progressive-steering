de633194a7a72877681d0bea4aa3b7387922a340
Mahog <6837928+Mahog@users.noreply.github.com>
2020-09-23 12:54:32 +0200

    add type definitions for coordinates in the plot

---
+++ frontend/src/PointTypes.ts
@@ -0,0 +1,3 @@
+export type ScaledCartesianCoordinate = { px: number, py: number };
+export type CartesianCoordinate = { x: number, y: number };
+export type PolarCoordinate = { r: number, theta: number };
