e2c99ef5bff6c17d72452b8d3f7f27e5b6677ba8
Mahog <6837928+Mahog@users.noreply.github.com>
2021-06-21 11:56:27 +0200

    update footer styling

--- frontend/src/Layout/Footer.tsx
+++ frontend/src/Layout/Footer.tsx
@@ -215,7 +215,7 @@ export default class Footer extends React.Component<Props, State> {
         <div className="right">
           <ProgressBar
             label="Progress"
-            width={ 210 }
+            width={ 95 }
             max={ this.props.dataAdapter.getTotalDataSize() }
             current={ this.props.dataAdapter.data.length }
           />
