07478902661c3f00def463b08833f6a46b2b4a8b
Mahog <6837928+Mahog@users.noreply.github.com>
2021-08-12 10:37:40 +0200

    use csv file per default as it speeds up row retrieval

--- backend/use_cases/nyc_taxis.py
+++ backend/use_cases/nyc_taxis.py
@@ -3,7 +3,7 @@ from typing import Any, List
 from use_cases.use_case import UseCase
 
 # source of dataset: https://data.cityofnewyork.us/Transportation/2018-Yellow-Taxi-Trip-Data/t29m-gskq
-FILE_PATH = "../data/nyc_taxis.parquet"
+FILE_PATH = "../data/nyc_taxis.csv"
 TABLE_NAME = "taxis"
 X_ENCODING = "trip_duration"
 Y_ENCODING = "tip_percentile"
